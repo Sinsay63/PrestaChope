dbaaf3671ffe8e545b70ca00c097fb106a5bf108
william carindo <Tigerwill63100@gmail.com>
2021-04-09 17:12:19 +0200

    footer

--- PrestaChope/index.php
+++ PrestaChope/index.php
@@ -5,6 +5,7 @@ session_start();
 
 require_once("tools/SuperController.php");
 require_once('header.php');
+require_once('footer.php');
 $page = "connexion";
 if (!empty($_GET['page'])) {
     $page = $_GET['page'];
