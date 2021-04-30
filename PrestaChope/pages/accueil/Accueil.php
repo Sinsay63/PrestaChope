ed0177dffa350cd96fce4019c5babd647d2606e6
william carindo <Tigerwill63100@gmail.com>
2021-04-30 17:13:20 +0200

    accueil debut

--- PrestaChope/pages/accueil/Accueil.php
+++ PrestaChope/pages/accueil/Accueil.php
@@ -1,25 +1,21 @@
 <html>
     <head>
         <title>Accueil</title>
-        <link rel="stylesheet" type="text/css" href="assets/css/nav.css">
         <link rel="stylesheet" type="text/css" href="assets/css/accueil.css">
     </head>
     <body>
         <main>
-            <div class="nav">
-                <div class="container-nav">
-                    
+            <div class="main">
+                <div class="main-container">
+                    <div class="main-content">
+                        <h1> La meilleur shop de bière</h1>
+                        <p>Venez vous laisser tenter par notre délicieuse bière</p>
+                        <button class="main-btn"><a href="#">C'est parti !</a></button>
+                    </div>
+                    <div class="main-img-container">
+                       <img src="assets/images/accueil-img.jpg" alt="" id="main-img">
+                    </div>
                 </div>
-            </div>
-            <div class="presentation">
-
-                <p> Bienvenue sur notre boutique de bières, nous vendons tout type de bières ainsi que des livraisons à domicile  </p>
-
-                <p>HORAIRES : Du lundi au vendredi de 8h à 18h</p>
-
-                <p>ADRESSE  : </p>
-
-                <p>HISTOIRE : PrestaChope est un projet réalisé par William CARINDO, Yanis HOUDIER et Thomas VIEIRA. Ce projet est une application de nos connaissances dans le codage </p>
             </div>
         </main>
     </body>
