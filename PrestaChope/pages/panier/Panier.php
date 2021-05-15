c54b6260caab42cc27386e7eb777fc67ce4e2711
william carindo <Tigerwill63100@gmail.com>
2021-05-15 19:12:28 +0200

    css panier

--- PrestaChope/pages/panier/Panier.php
+++ PrestaChope/pages/panier/Panier.php
@@ -1,6 +1,9 @@
 <html>
     <head>
         <title>Panier</title>
+        <link rel="stylesheet" href="assets/css/panier.css"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     </head>
     <body>
         <?php
@@ -12,25 +15,41 @@
                 }
                 $user = UsersDAO::GetUserInfo($_SESSION['ID']);
                 ?>
-                <p>Votre solde est de <?php echo $user->getCagnotte(); ?>€</p>
+                <div class="text_solde">
+                    <p>Votre solde est de <?php echo $user->getCagnotte(); ?>€</p>
+                </div>
                 <br>
-                <p>Mon panier:<p> <br><?php
+                <div class="text_panier">
+                <p>Mon panier :
+                </div>
+                <div class="text_article">
+                 <p> <br><?php
+
                     foreach ($panier as $key => $value) {
                         $produit = ProduitsDAO::searchProductsById($value->getIdproduit());
-                        echo 'Article : ' . $produit->getNom() . ' -> quantité : ' . $value->getQuantité() . ' à un prix de ' . $produit->getPrix() . '€ l\'unité.';
+                        echo 'Article : ' . $produit->getNom() . ' ; quantité : ' . $value->getQuantité() . ' à un prix de ' . $produit->getPrix() . '€ l\'unité.';
                         ?>
                         <a href="index.php?page=deletePanier&del=<?php echo $value->getId() ?>">Supprimer du panier</a>
                         <br><br>
+                </div>
+
           <?php } ?>
-                <p> Le montant total du panier est de <?php echo ControllerPanier::getMontantPanier($_SESSION['ID']) ?>€.</p> <br>
-                <a href="index.php?page=commander">Commander</a>
+                <div class="text_montant">
+                    <p> Le montant total du panier est de <?php echo ControllerPanier::getMontantPanier($_SESSION['ID']) ?>€.</p> <br>
+                </div>
+                <div class="text_commander">
+                    <a href="index.php?page=commander">Commander</a>
+                </div>
             <?php
             } 
             else {  ?>
                 <p>Votre panier est vide </p>
                 <?php
             }
+
         }
         ?>
+    
+
     </body>
 </html>
