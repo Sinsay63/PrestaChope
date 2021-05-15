f6770d23d633201af23187d8716aa184fc05e7bc
william carindo <Tigerwill63100@gmail.com>
2021-05-15 18:34:39 +0200

    css profil suite

--- PrestaChope/pages/profil/Profil.php
+++ PrestaChope/pages/profil/Profil.php
@@ -29,18 +29,18 @@
                     <div class="prénom">
                         <p id="prénoms"><?php echo $user->getPrénom(); ?><p>
                         <form id="form1" method="post" action="index.php?page=modifProfil">
-                            <input type="text" name="info" placeholder="<?php echo $user->getPrénom(); ?>"/>
+                            <input type="text" name="info" placeholder="<?php echo $user->getPrénom(); ?>" id="espace2"/>
                             <input type="hidden" name="type" value="prenom">
-                            <input type="submit" value="Modifier" id="btn1"/>
+                            <input type="submit" value="Modifier" class="btn1"/>
                         </form>
                         <input type="button" id="btn1" value="Modifier" onclick="hideThis('form1', 'btn1', 'prénoms')" />
                     </div>
                     <div class="nom">
                         <p id="noms"><?php echo $user->getNom(); ?><p>
                         <form id="form2" method="post" action="index.php?page=modifProfil">
-                            <input type="text" name="info" placeholder="<?php echo $user->getNom(); ?>"/>
+                            <input type="text" name="info" placeholder="<?php echo $user->getNom(); ?>" id="espace3"/>
                             <input type="hidden" name="type" value="nom">
-                            <input type="submit" value="Modifier" id="btn2"/>
+                            <input type="submit" value="Modifier" class="btn2"/>
                         </form>
                         <input type="button" id="btn2" value="Modifier" onclick="hideThis('form2', 'btn2', 'noms')" />
                     </div>
@@ -51,29 +51,29 @@
                         <div class="email">
                             Votre email:<p id="emails"><?php echo $user->getEmail(); ?><p>
                             <form id="form3" method="post" action="index.php?page=modifProfil">
-                                <input type="text" name="info" placeholder="<?php echo $user->getEmail(); ?>"/>
+                                <input type="text" name="info" placeholder="<?php echo $user->getEmail(); ?>" id="espace1"/>
                                 <input type="hidden" name="type" value="email">
-                                <input type="submit" value="Modifier" id="btn3"/>
+                                <input type="submit" value="Modifier" class="btn3"/>
                             </form>
                             <input type="button" id="btn3" value="Modifier" onclick="hideThis('form3', 'btn3', 'emails')" />
                         </div>
                         <div class="pseudo">
                             Votre pseudo:<p id="pseudos"><?php echo $user->getPseudo(); ?><p>
                             <form id="form4" method="post" action="index.php?page=modifProfil">
-                                <input type="text" name="info" placeholder="<?php echo $user->getPseudo(); ?>"/>
+                                <input type="text" name="info" placeholder="<?php echo $user->getPseudo(); ?>"id="espace1"/>
                                 <input type="hidden" name="type" value="pseudo">
-                                <input type="submit" value="Modifier" id="btn4"/>
+                                <input type="submit" value="Modifier" class="btn4"/>
                             </form>
                             <input type="button" id="btn4" value="Modifier" onclick="hideThis('form4', 'btn4', 'pseudos')" />
                         </div>
                         <div class="password">
                             Votre mot de passe:<p id="passwords"><p>
                             <form id="form9" method="post" action="index.php?page=modifProfil">
-                                <input type="password" name="info[]" placeholder="Ancien mot de passe"/>
-                                <input type="password" name="info[]" placeholder="Nouveau mot de passe"/>
-                                <input type="password" name="info[]" placeholder="Retapez mot de passe" />
+                                <input type="password" name="info[]" placeholder="Ancien mot de passe"id="espace1"/>
+                                <input type="password" name="info[]" placeholder="Nouveau mot de passe"id="espace1"/>
+                                <input type="password" name="info[]" placeholder="Retapez mot de passe" id="espace1"/>
                                 <input type="hidden" name="type" value="password">
-                                <input type="submit" value="Modifier" id="btn5"/>
+                                <input type="submit" value="Modifier" class="btn5"/>
                             </form>
                             <input type="button" id="btn5" value="Modifier" onclick="hideThis('form9', 'btn5', 'password')" />
                         </div>
@@ -85,20 +85,20 @@
                             <div class="adresse">
                                 Votre adresse:<p id="adresses"><?php echo $client->getAdresse() . ' - ' . $client->getVille() . ' - ' . $client->getCp(); ?><p>
                                 <form id="form5" method="post" action="index.php?page=modifProfil">
-                                    <input type="text" name="info[]" placeholder="<?php echo $client->getAdresse(); ?>"/>
-                                    <input type="text" name="info[]" placeholder="<?php echo $client->getVille(); ?>"/>
-                                    <input type="text" name="info[]" placeholder="<?php echo $client->getCp(); ?>"/>
+                                    <input type="text" name="info[]" placeholder="<?php echo $client->getAdresse(); ?>" id="espace1"/>
+                                    <input type="text" name="info[]" placeholder="<?php echo $client->getVille(); ?>"id="espace1"/>
+                                    <input type="text" name="info[]" placeholder="<?php echo $client->getCp(); ?>"id="espace1"/>
                                     <input type="hidden" name="type" value="adresse">
-                                    <input type="submit" value="Modifier" id="btn6"/>
+                                    <input type="submit" value="Modifier" class="btn6"/>
                                 </form>
                                 <input type="button" id="btn6" value="Modifier" onclick="hideThis('form5', 'btn6', 'adresses')" />
                             </div>
                             <div class="téléphone">
                                 Votre numéro de téléphone:<p id="telephones"><?php echo $client->getTelephone(); ?><p>
                                 <form id="form6" method="post" action="index.php?page=modifProfil">
-                                    <input type="text" name="info" placeholder="<?php echo $client->getTelephone(); ?>"/>
+                                    <input type="text" name="info" placeholder="<?php echo $client->getTelephone(); ?>" id="espace1"/>
                                     <input type="hidden" name="type" value="telephone">
-                                    <input type="submit" value="Modifier" id="btn7"/>
+                                    <input type="submit" value="Modifier" class="btn7"/>
                                 </form>
                                 <input type="button" id="btn7" value="Modifier" onclick="hideThis('form6', 'btn7', 'telephones')" />
                             </div>
@@ -108,11 +108,11 @@
                         <div class="adresse">
                             Votre adresse:
                             <form id="form7" method="post" action="index.php?page=modifProfil">
-                                <input type="text" name="info[]" placeholder="Adresse"/>
-                                <input type="text" name="info[]" placeholder="Ville"/>
-                                <input type="text" name="info[]" placeholder="Code postal"/>
+                                <input type="text" name="info[]" placeholder="Adresse" id="espace1"/>
+                                <input type="text" name="info[]" placeholder="Ville" id="espace1"/>
+                                <input type="text" name="info[]" placeholder="Code postal" id="espace1"/>
                                 <input type="hidden" name="type" value="adresse">
-                                <input type="submit" value="Ajouter" id="btn8"/>
+                                <input type="submit" value="Ajouter" class="btn8"/>
                             </form>
                             <input type="button" id="btn8" value="Ajouter" onclick="hideThis('form7', 'btn8', '')" />
                         </div>
@@ -120,9 +120,9 @@
                         <div class="téléphone">
                             Votre numéro de téléphone:
                             <form id="form8" method="post" action="index.php?page=modifProfil">
-                                <input type="text" name="info" placeholder="Votre numéro de téléphone"/>
+                                <input type="text" name="info" placeholder="Votre numéro de téléphone" id="espace1"/>
                                 <input type="hidden" name="type" value="telephone">
-                                <input type="submit" value="Ajouter" id="btn9"/>
+                                <input type="submit" value="Ajouter" class="btn9"/>
                             </form>
                             <input type="button" id="btn9" value="Ajouter" onclick="hideThis('form8', 'btn9', '')" />
                         </div>
@@ -137,7 +137,7 @@
             </div>
         </div>
 
-                
+                    
                 <div class="delete_user">
                     <a href="index.php?page=deleteProfil&delete=<?php echo $_SESSION['ID']; ?>">Supprimer le compte</a>
                 </div>
