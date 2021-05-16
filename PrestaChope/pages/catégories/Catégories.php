6cdb89aab3f5f2dfe79e975a73d9944edcf2cc63
vosterss <77280391+vosterss@users.noreply.github.com>
2021-05-16 23:58:59 +0200

    cate

--- PrestaChope/pages/catégories/Catégories.php
+++ PrestaChope/pages/catégories/Catégories.php
@@ -3,6 +3,15 @@
         <title></title>
         <script type="text/javascript" src="assets/js/catégories.js" async></script>
         <style>
+            body{
+                background-color: rgb(215, 219, 221);
+            }
+
+            main{
+                padding-right: 150px; 
+                padding-left: 150px; 
+            }
+            
             a{
                 text-decoration: none;
                 color:black;
@@ -10,40 +19,44 @@
         </style>
     </head>
     <body>
-        <?php
-        $allCaté = CatégoriesDAO::SearchAllCatégories_SousCatégories();
-        if ($allCaté != null) {
-            foreach ($allCaté as $value) {
+        <main>
+            <?php
+            $allCaté = CatégoriesDAO::SearchAllCatégories_SousCatégories();
+            if ($allCaté != null) {
+                foreach ($allCaté as $value) {
+                    ?>
+                    <a href="index.php?page=modifCatégorie&caté=<?php echo $value->getId(); ?>"> 
+                        <p>Catégorie : <?php echo $value->getNom(); ?> </p><br>
+                        <p>Description : <?php echo $value->getDescription(); ?></p><br>
+                        <p>Sous catégories:</p>
+                        <?php
+                        if (!empty($value->getSouscatégories())) {
+
+                            foreach ($value->getSouscatégories() as $souscaté) {
+                                ?>
+                                <li> <?php echo $souscaté->getNom(); ?></li>
+                                <?php
+                            }
+                        } else {
+                            echo 'Cette catégorie n\'a pas de sous-catégorie';
+                        }
+                        ?>
+                    </a>
+                    <br>
+                    <a href="index.php?page=deleteCatégorie&id=<?php echo $value->getId(); ?>">Supprimer la catégorie : <?php echo $value->getNom(); ?></a><br><br>
+                    <?php
+                }
                 ?>
-                <a href="index.php?page=modifCatégorie&caté=<?php echo $value->getId(); ?>"> 
-                    <p>Catégorie : <?php echo $value->getNom(); ?> </p><br>
-                    <p>Description : <?php echo $value->getDescription(); ?></p><br>
-                    <p>Sous catégories:</p>
-                    <?php 
-                    if(!empty($value->getSouscatégories())){
-                        
-                    foreach ($value->getSouscatégories() as $souscaté) { ?>
-                        <li> <?php echo $souscaté->getNom(); ?></li>
-                    <?php }
-                    } 
-                    else{
-                        echo 'Cette catégorie n\'a pas de sous-catégorie';
-                    } ?>
-                </a>
-                <br>
-                <a href="index.php?page=deleteCatégorie&id=<?php echo $value->getId(); ?>">Supprimer la catégorie : <?php echo $value->getNom(); ?></a><br><br>
+                <a href="index.php?page=créationCatégorie">Ajouter une nouvelle catégorie </a>
+                <?php
+            } else {
+                ?>
+                <p>Aucune catégorie n'a été trouvé.</p>
+                <a href="index.php?page=créationCatégorie">Ajouter une nouvelle catégorie </a>
                 <?php
             }
             ?>
-            <a href="index.php?page=créationCatégorie">Ajouter une nouvelle catégorie </a>
-            <?php
-        } else {
-            ?>
-            <p>Aucune catégorie n'a été trouvé.</p>
-            <a href="index.php?page=créationCatégorie">Ajouter une nouvelle catégorie </a>
-            <?php
-        }
-        ?>
+        </main>
     </body>
 </html>
 
