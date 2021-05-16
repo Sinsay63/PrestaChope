4544b102cf645e53b1befa4f34e7b47e89ece523
vosterss <77280391+vosterss@users.noreply.github.com>
2021-05-16 22:15:56 +0200

    message / uti

--- PrestaChope/header.php
+++ PrestaChope/header.php
@@ -25,56 +25,68 @@
                 </div>
                 <?php
                 if (!empty($_SESSION['ID'])) {
-                    ?>
-                    <div class="bouton-contact">
-                        <a id="nav" href="index.php?page=contact">           	
-                            <button class="bouton-Co" type="button">
-                                <p class="txt-bouton">Contact</p>
-                            </button>
-                        </a>
-                    </div>
-                    <div class="bouton-profil">
-                        <a id="nav" href="index.php?page=profil">           	
-                            <button class="bouton-P" type="button">
-                                <p class="txt-bouton">Profil</p>
-                            </button>
-                        </a>
-                    </div>
-                    <div class="bouton-deco">
-                        <a id="nav" href="index.php?page=deconnexion">           	
-                            <button class="bouton-D" type="button">
-                                <p class="txt-bouton">Log Out</p>
-                            </button>
-                        </a>
-                    </div>
-                    
-                    <?php
-                    require_once('DAO/UsersDAO.php');
-                    require_once('DAO/FacturesDAO.php');
+                    if ($_SESSION['IsAdmin'] == 0) {
+                        ?>
+                        <div class="bouton-contact">
+                            <a id="nav" href="index.php?page=contact">           	
+                                <button class="bouton-Co" type="button">
+                                    <p class="txt-bouton">Contact</p>
+                                </button>
+                            </a>
+                        </div>
+                    <?php }
                     if ($_SESSION['IsAdmin'] == 1) {
-                        echo "<p class='txt-bouton-A'>Trésorerie : ". FacturesDAO::TotalTresorerie(). "€</p>";
+                        ?>
+                        <div class="bouton-uti">
+                            <a id="nav" href="index.php?page=listeUtilisateurs">           	
+                                <button class="bouton-U" type="button">
+                                    <p class="txt-bouton">Utilisateurs</p>
+                                </button>
+                            </a>
+                        </div>
+        <?php } ?>
+                        <div class="bouton-profil">
+                            <a id="nav" href="index.php?page=profil">           	
+                                <button class="bouton-P" type="button">
+                                    <p class="txt-bouton">Profil</p>
+                                </button>
+                            </a>
+                        </div>
+                        <div class="bouton-deco">
+                            <a id="nav" href="index.php?page=deconnexion">           	
+                                <button class="bouton-D" type="button">
+                                    <p class="txt-bouton">Log Out</p>
+                                </button>
+                            </a>
+                        </div>
+
+                        <?php
+                        require_once('DAO/UsersDAO.php');
+                        require_once('DAO/FacturesDAO.php');
+                        if ($_SESSION['IsAdmin'] == 1) {
+                            echo "<p class='txt-bouton-A'>Trésorerie : " . FacturesDAO::TotalTresorerie() . "€</p>";
+                        }
+                        $user = new UsersDAO();
+                        $us = $user->GetUserInfo($_SESSION['ID']);
+                    } else {
+                        ?>
+                        <div class="bouton-register">
+                            <a id="nav" href="index.php?page=inscription">           	
+                                <button class="bouton-R" type="button">
+                                    <p class="txt-bouton">Register</p>
+                                </button>
+                            </a>
+                        </div>
+                        <div class="bouton-login">
+                            <a id="nav" href="index.php?page=connexion">         
+                                <button class="bouton-L" type="button">
+                                    <p class="txt-bouton">Log In</p>
+                                </button>
+                            </a>
+                        </div>	
+                        <?php
                     }
-                    $user = new UsersDAO();
-                    $us = $user->GetUserInfo($_SESSION['ID']);
-                } else {
                     ?>
-                    <div class="bouton-register">
-                        <a id="nav" href="index.php?page=inscription">           	
-                            <button class="bouton-R" type="button">
-                                <p class="txt-bouton">Register</p>
-                            </button>
-                        </a>
-                    </div>
-                    <div class="bouton-login">
-                        <a id="nav" href="index.php?page=connexion">         
-                            <button class="bouton-L" type="button">
-                                <p class="txt-bouton">Log In</p>
-                            </button>
-                        </a>
-                    </div>	
-                    <?php
-                }
-                ?>
             </div>
         </div>
     </body>
