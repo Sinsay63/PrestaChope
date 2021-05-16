069e26b565b02d83ae73ac12597c7e4ad96ab1f2
Sinsay63 <yanis.houdier@gmail.com>
2021-05-16 23:57:58 +0200

    push final

--- PrestaChope/pages/catégories/Catégories.php
+++ PrestaChope/pages/catégories/Catégories.php
@@ -2,6 +2,9 @@
     <head>
         <title></title>
         <script type="text/javascript" src="assets/js/catégories.js" async></script>
+        <link rel="stylesheet" type="text/css" href="assets/css/categories.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
         <style>
             a{
                 text-decoration: none;
@@ -10,15 +13,17 @@
         </style>
     </head>
     <body>
+        <div class="prod">
         <?php
         $allCaté = CatégoriesDAO::SearchAllCatégories_SousCatégories();
         if ($allCaté != null) {
             foreach ($allCaté as $value) {
                 ?>
-                <a href="index.php?page=modifCatégorie&caté=<?php echo $value->getId(); ?>"> 
-                    <p>Catégorie : <?php echo $value->getNom(); ?> </p><br>
+                <div class="align">
+                <p>Catégorie : <?php echo $value->getNom(); ?> </p><br>
                     <p>Description : <?php echo $value->getDescription(); ?></p><br>
-                    <p>Sous catégories:</p>
+                    <p>Sous catégories:</p>  
+
                     <?php 
                     if(!empty($value->getSouscatégories())){
                         
@@ -29,13 +34,20 @@
                     else{
                         echo 'Cette catégorie n\'a pas de sous-catégorie';
                     } ?>
+                <a href="index.php?page=modifCatégorie&caté=<?php echo $value->getId(); ?>">
+                <button class="button button1">Modifier cette catégorie</button>
+                    
+                    
                 </a>
                 <br>
-                <a href="index.php?page=deleteCatégorie&id=<?php echo $value->getId(); ?>">Supprimer la catégorie : <?php echo $value->getNom(); ?></a><br><br>
+                <a href="index.php?page=deleteCatégorie&id=<?php echo $value->getId(); ?>"><button class="button button2">Supprimer cette catégorie</button></a><br><br>
+            </div>
                 <?php
+
             }
+
             ?>
-            <a href="index.php?page=créationCatégorie">Ajouter une nouvelle catégorie </a>
+            <a href="index.php?page=créationCatégorie"><button class="button button3">Ajouter une catégorie</button></a>
             <?php
         } else {
             ?>
@@ -44,6 +56,7 @@
             <?php
         }
         ?>
+    </div>
     </body>
 </html>
 
