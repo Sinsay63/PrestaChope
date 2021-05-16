c97c9ccdfb5cb23bb60b69f9bdfd79fcb20ba289
william carindo <Tigerwill63100@gmail.com>
2021-05-16 18:53:59 +0200

    css catalogue et inscription

--- PrestaChope/pages/inscription/Inscription.php
+++ PrestaChope/pages/inscription/Inscription.php
@@ -1,6 +1,9 @@
-<html>
+        <html>
     <head>
         <title>Inscription</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/inscription.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     </head>
     <body>
         <?php
@@ -21,23 +24,71 @@
             }
         }
         ?>
+        <div class="erreur">
+        <p>       <p>
+        </div>
+        <div id="container">
+
         <form action="" method="post">
+            <div class="inscr">
+            <p> Inscription <p>
+            </div>
+            <div class="nomprenom">
+            <div class="nom">
+             <label><b>Votre nom</b></label>
+            
+            
             <input type="text" name="nom" placeholder="entrez votre nom..." required />
+            </div>
+            <div class="prenom">
+            <label><b>Votre prénom</b></label>
             <input type="text" name="prénom" placeholder="entrez votre prénom..." required/>
+            </div>
+        </div>
+            <div class="emailpseudo">
+            <div class="email">
+            <label><b>Votre email</b></label>
             <input type="text" name="email" placeholder="entrez votre email..." required/>
+            </div>
+            
+            <div class="pseudo">
+            <label><b>Votre pseudo</b></label>
+            <input type="text" name="pseudo" placeholder="entrez votre pseudo..." required/>
+            </div>
+        </div>
+            <div class="mdpconf">
+            <div class="mot_de_passe">
+            <label><b>Votre mot de passe</b></label>
+            <input type="password" name="password" placeholder="entrez votre mot de passe..." required=""/>
+            </div>
+
+            <div class="confmdp">
+            <label>Confirmez</label>
+            <input type="password" name="confirm_password" placeholder="Confirmation mot de passe..." required/>
+        </div>
+    
+    </div> 
+            <div class="age">
+            <label><b>Votre age</b></label>
+
+        
             <select name="age" style="width:90px;" required>
+
                 <option value="" hidden>Votre âge</option>
+
+
                 
         <?php   for ($i = 18; $i < 100; $i++) { ?>
                     <option value="<?php echo $i ?>"> <?php echo $i . " ans" ?> </option>
         <?php   }
                 ?>
+                
             </select>
-            <input type="text" name="pseudo" placeholder="entrez votre pseudo..." required/>
-            <input type="password" name="password" placeholder="entrez votre mot de passe..." required=""/>
-            <input type="password" name="confirm_password" placeholder="Confirmation mot de passe..." required/>
-            <input type="submit" value="S'inscrire">            
+            </div>   
+            <input type="submit" value="S'inscrire" >
+                   
         </form>
+    </div>
     </body>
 </html>
 
